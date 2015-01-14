da9cf46b6ad0a31e339b29584af789522303d4ae
420303844@qq.com <admin_123>
2015-01-14 15:51:11 +0800

    ...

---
+++ js/detail.js
@@ -0,0 +1,57 @@
+var audio = document.getElementById("myMusic");
+
+$(function(){
+    new FastClick(document.body);	
+	var clo = document.getElementById("close");
+	clo.onclick = function(){
+		document.getElementById("audio_top").style.display = "none";	
+	}
+		
+	var speed = 20;
+	var direction="left";
+	var tab = document.getElementById("txtBox");
+	var txt1 = document.getElementById("txt1");
+	var txt2 = document.getElementById("txtCopy");
+	function marquee(){
+		switch(direction){
+			case "left":
+				if(txt2.offsetWidth - tab.scrollLeft <= 0){
+					tab.scrollLeft -= txt1.offsetWidth;
+				} 
+				else{
+					tab.scrollLeft++;
+				}
+			break;
+			case "right":
+				if(tab.scrollLeft <= 0){
+					tab.scrollLeft += txt2.offsetWidth;
+				}
+				else{
+					tab.scrollLeft--;
+				}
+			break;
+		}
+	}
+	if (txt1.offsetWidth > tab.offsetWidth) {
+		txt2.innerHTML = txt1.innerHTML;
+		var timer = setInterval(marquee,speed);
+		tab.onmouseover = function(){clearInterval(timer);};
+		tab.onmouseout = function(){timer = setInterval(marquee,speed);}; 
+	}
+	
+	
+	$("#play").on("click",function(){
+		var flag = $(this).hasClass("stop");
+		if(flag){
+			$(this).removeClass("stop").addClass("play");
+			if (audio.src == "") {
+				//audio.src = Defaultsong
+			}
+			audio.play();	
+		}else{
+			$(this).removeClass("play").addClass("stop");
+        	audio.pause();
+		}
+	})
+	
+});
---
+++ js/live_audio.js
@@ -0,0 +1,132 @@
+var audio = document.getElementById("myMusic"),
+p = $(".Process").offset(),
+ps = p.left,
+pl = $(".Process").width();
+var CurrentProces = null;
+$(document).ready(function () { 
+	var Defaultsong = $(".Single .SongName").eq(0).attr("KV");
+	var songName = $(".Single .SongName").eq(0).attr("SN");
+	$(".SongName").text(songName);
+	audio.src = Defaultsong;
+	audio.play();
+	$("#MainControl").on("click",function(){
+		var flag = $(this).hasClass("MainControl");
+		if(flag){
+			$(this).removeClass("MainControl").addClass("StopControl");
+			audio.play();
+			TimeSpan();	
+		}else{
+			$(this).removeClass("StopControl").addClass("MainControl");
+        	audio.pause();
+		}
+	})
+
+    $(".ProcessControl").click(function (e) {
+
+        CurrentProces = e.clientX - ps;
+        DurationProcessRange(CurrentProces / pl);
+        $(".ProcessYet").css("width", CurrentProces);
+    });
+	var _btn = document.getElementById("dotBtn");
+	var moveX = null;
+	_btn.addEventListener("touchmove",function(event){
+		event.preventDefault();
+		var touch = event.touches[0];
+    	moveX = touch.pageX;
+		CurrentProces = (moveX - ps)>180?180:(moveX - ps);
+        $(".ProcessYet").css("width", CurrentProces);
+		 
+	}, false);
+	
+	_btn.addEventListener("touchend",function(event){
+		event.preventDefault();
+		//var touch = event.touches[0],
+    	//endX = touch.pageX;
+		console.log(moveX);
+		CurrentProces = (moveX - ps)>180?180:(moveX - ps);
+        DurationProcessRange(CurrentProces / pl);
+        $(".ProcessYet").css("width", CurrentProces);
+		 
+	}, false);
+
+
+    //显示音乐列表事件
+    $(".ShowMusicList").toggle(function () {
+        $(".MusicList").show();
+
+        var MusicBoxRight = $(".MusicBox").offset().left + $(".MusicBox").width();
+        var MusicBoxBottom = $(".MusicBox").offset().top + $(".MusicBox").height();
+        $(".MusicList").css("left", MusicBoxRight - $(".MusicList").width());
+        $(".MusicList").css("top", MusicBoxBottom + 15);
+    }, function () {
+        $(".MusicList").hide();
+    });
+
+
+    //监听媒体结束
+    audio.addEventListener('ended', function () {
+        pop_box("直播已结束 将为您播放录播节目");
+    }, false);
+
+
+});
+
+function DurationProcessRange(rangeVal) {
+    
+    audio.currentTime = rangeVal * audio.duration;
+	if($("#MainControl").hasClass("StopControl")){
+    	audio.play();
+	}
+}
+
+//播放事件
+function Play(obj) {
+	  var SongUrl = obj.getAttribute("SongUrl");
+    
+    audio.src = SongUrl
+    audio.play();
+    TimeSpan();
+}
+
+function Pause() {
+    
+    $("#PauseTime").val(audio.currentTime);
+    audio.pause();
+}
+
+
+function Continue() {
+    audio.startTime = $("PauseTime").val();
+    audio.play();
+}
+
+function TimeSpan() {
+    
+    var ProcessYet = 0;
+	
+    setInterval(function () {
+        var ProcessYet = (audio.currentTime / audio.duration) * 180;
+        $(".ProcessYet").css("width", ProcessYet);
+        var currentTime = timeDispose(audio.currentTime);
+		var timeAll = timeDispose(TimeAll());
+		$(".curTime").html(currentTime);
+		$(".tolTime").html(timeAll);
+    }, 500);
+	
+}
+
+function timeDispose(number) {
+		var minute = parseInt(number / 60);
+		var second = parseInt(number % 60);
+		minute = minute >= 10 ? minute : "0" + minute;
+		second = second >= 10 ? second : "0" + second;
+		if(minute == "0NaN"){return "00:00"}
+		else{
+		return minute + ":" + second;
+		}
+}
+
+
+function TimeAll() {
+    return audio.duration;
+}
